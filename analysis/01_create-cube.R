3d67e6ee7c077236269231f8ba18cb08b3717ed7
Felipe Carlos <efelipecarlos@gmail.com>
2025-01-28 16:57:38 -0300

    global: initial commit

---
+++ analysis/01_create-cube.R
@@ -0,0 +1,96 @@
+set.seed(777)
+
+library(sf)
+library(fs)
+library(sits)
+library(dplyr)
+
+#
+# Cube definitions
+#
+
+# Cube dates
+cube_years <- 2014:2020
+
+# Temporal composition
+cube_temporal_composition <- "P3M"
+
+# Bands
+cube_bands <- c("BLUE", "GREEN", "RED", "NIR08" , "SWIR16", "SWIR22", "CLOUD")
+
+# Cube directory
+cube_base_dir <- "data/derived/cube"
+
+# Region
+cube_region_file <- "data/raw/region/amazon-regions-bdc-md.gpkg"
+
+#
+# Hardware definitions
+#
+
+# Multicores
+multicores <- 64
+
+# Memory size
+memsize    <- 220
+
+#
+# 1. Create cube directory
+#
+fs::dir_create(cube_base_dir, recurse = TRUE)
+
+#
+# 2. Load region file
+#
+region <- st_read(cube_region_file)
+
+#
+# 3. Filter region for region number 4
+#
+region <- filter(region, layer == "eco_4")
+
+region <- st_union(st_convex_hull(region))
+
+#
+# 4. Generate cubes
+#
+for (cube_year in cube_years) {
+  print(paste0("Processing: ", cube_year))
+
+  #
+  # 3.1. Define cube dates
+  #
+  start_date <- paste0(cube_year, "-01-01")
+  end_date   <- paste0(cube_year, "-12-31")
+
+  #
+  # 3.2. Create a directory for the current year
+  #
+  cube_dir_year <- path(cube_base_dir) / cube_year
+
+  fs::dir_create(cube_dir_year, recurse = TRUE)
+
+  #
+  # 3.3. Load cube
+  #
+  cube_year <- sits_cube(
+    source     = "MPC",
+    collection = "LANDSAT-C2-L2",
+    roi        = region,
+    start_date = start_date,
+    end_date   = end_date,
+    bands      = cube_bands
+  )
+
+  #
+  # 3.4. Regularize
+  #
+  reg_cube <- sits_regularize(
+    cube        = cube_year,
+    period      = cube_temporal_composition,
+    res         = 30,
+    multicores  = multicores,
+    output_dir  = cube_dir,
+    grid_system = "BDC_MD_V2"
+  )
+}
