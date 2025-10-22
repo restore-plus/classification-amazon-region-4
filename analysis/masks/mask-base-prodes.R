00d5c85cbb7cdd439b8f7662fface9f875dbca21
Felipe <lipecaso@gmail.com>
2025-10-22 18:26:19 +0000

    update terraclass and prodes mask generation

--- analysis/masks/mask-base-prodes.R
+++ analysis/masks/mask-base-prodes.R
@@ -2,43 +2,34 @@ library(sits)
 library(restoreutils)
 
 #
-# Config: Connection timeout
-#
-options(timeout = max(600, getOption("timeout")))
-
-
-#
 # General definitions
 #
-memsize    <- 30
-multicores <- 10
+memsize    <- 100
+multicores <- 35
 
 version <- "v2"
 
-mask_years <- c(2006, 2016, 2021)
-
+years_to_prepare <- c(2000:2007, 2024)
+years_to_mask <- c(2000:2024)
 
 #
 # 1) Download Prodes data
 #
 restoreutils::prepare_prodes(
-    region_id = 4
+  region_id = 4,
+  years     = years_to_prepare
 )
 
 
 #
 # 2) Generate forest mask
 #
-# Note: We start generating masks in 2023, as 2024 is the most recent data, and
-#       all forest there is the current forest. So, there is no requirements for
-#       extra data transformations
-#
-purrr::map(mask_years, function(mask_year) {
-  restoreutils::prodes_generate_forest_mask(
-    target_year   = mask_year,
-    version       = version,
-    multicores    = multicores,
-    memsize       = memsize
+purrr::map(years_to_mask, function(mask_year) {
+  restoreutils::prodes_generate_mask(
+    target_year    = mask_year,
+    version        = version,
+    multicores     = multicores,
+    memsize        = memsize,
+    nonforest_mask = TRUE
   )
 })
-
--- analysis/masks/mask-base-terraclass.R
+++ analysis/masks/mask-base-terraclass.R
@@ -2,16 +2,23 @@ library(sits)
 library(restoreutils)
 
 #
-# Config: Connection timeout
+# General definitions
 #
-options(timeout = max(600, getOption("timeout")))
+memsize    <- 100
+multicores <- 35
 
+year_to_prepare <- c(2004, 2008, 2010, 2012, 2014, 2018, 2020, 2022)
 
 #
 # 1) Download Terraclass data
 #
 restoreutils::prepare_terraclass(
-    years      = c(2010),
-    region_id  = 4,
-    multicores = 5
+    years            = year_to_prepare,
+    region_id        = 4,
+    multicores       = multicores,
+    memsize          = memsize,
+    fix_other_uses   = TRUE,
+    fix_urban_area   = TRUE,
+    fix_non_forest   = TRUE,
+    fix_non_observed = TRUE
 )
