061a0d6a031c414b01ffcb65b05e0c0b501ef8d2
Felipe <lipecaso@gmail.com>
2025-10-22 21:34:13 +0000

    update rule temporal with the right script

--- analysis/masks/mask-temporal.R
+++ analysis/masks/mask-temporal.R
@@ -12,15 +12,15 @@ base_masks_dir <- restoreutils::project_masks_dir()
 mask_version <- "rules-latest"
 
 # Hardware - Multicores
-multicores <- 40
+multicores <- 35
 
 # Hardware - Memory size
-memsize <- 180
+memsize <- 100
 
 #
 # 1. Generate directories
 #
-output_dir <- create_data_dir(base_masks_dir / mask_version, "temporal-allyears")
+output_dir <- create_data_dir(base_masks_dir / mask_version, "results")
 
 
 #
@@ -34,8 +34,8 @@ files <- restoreutils::get_restore_rds_files(mask_version)
 
 # Define output dir
 water_year <- 2019
-water_output_dir <- base_masks_dir / water_year
-water_version < - "temporal-mask-1"
+water_version <- "temporal-mask-1"
+water_output_dir <- output_dir / water_version / water_year
 
 # Define years to apply
 water_mask_years <- c(2018, 2019, 2020)
@@ -45,7 +45,7 @@ files_water <- files[files[["year"]] %in% water_mask_years,]
 
 # Reclassify water 
 water_outfile <- restoreutils::reclassify_rule24_temporal_water_consistency(
-    files          = files_water,
+    files          = files_water[["path"]],
     water_class_id = 3,
     year           = water_year,
     version        = water_version,
@@ -55,7 +55,7 @@ water_outfile <- restoreutils::reclassify_rule24_temporal_water_consistency(
 )
 
 # Update water file
-files[files[["year"]] == water_year,] <- water_outfile
+files[files[["year"]] == water_year, "path"] <- water_outfile
 
 #
 # 4. Apply perene mask
@@ -64,11 +64,12 @@ files[files[["year"]] == water_year,] <- water_outfile
 # Define years to apply
 perene_mask_years <- c(2009, 2011, 2013, 2019, 2021)
 perene_version <- "temporal-mask-2"
+perene_label <- "Perene"
 
 # Apply perene rules
 perene_outfiles <- sapply(perene_mask_years, function(year) {
     # Define output dir
-    perene_output_dir <- base_masks_dir / year
+    perene_output_dir <- output_dir / perene_version / year
     
     # Load after terraclass
     tc_after <- get(paste0("load_terraclass_", year + 1))
@@ -81,17 +82,19 @@ perene_outfiles <- sapply(perene_mask_years, function(year) {
     tc_before <- tc_before[["file_info"]][[1]][["path"]]
     
     # Filter current year
-    year_file <- files[files[["year"]] == year, "path"]
+    year_file <- files[files[["year"]] == year, "path"][["path"]]
     
     # Filter label value
-    class_value <- files[files[["year"]] == year, "label"][[1]]["Perene"]
+    class_value <- files[files[["year"]] == year, "labels"][[1]][[1]]
+    class_value <- as.numeric(names(class_value[class_value == perene_label]))
 
+    # Apply rule perene trajectory rule
     restoreutils::reclassify_rule27_temporal_trajectory_perene_mask(
         files                = year_file,
         files_mask           = c(tc_before, tc_after),
         year                 = year,
         perene_class_id      = class_value,
-        perene_mask_class_id = 12,
+        perene_mask_class_id = 12, # 12 means Perene in terraclass maps
         version              = perene_version,
         multicores           = multicores,
         memsize              = memsize,
@@ -100,14 +103,16 @@ perene_outfiles <- sapply(perene_mask_years, function(year) {
 })
 
 # Update perene files
-files[files[["year"]] == perene_mask_years,] <- perene_outfiles
+files[files[["year"]] == perene_mask_years, "path"] <- perene_outfiles
 
 #
-# 5. Reclassify agriculture neighbor 
+# 5. Reclassify agriculture neighbor
 #
 
 # Definitions
 agri_version <- "temporal-mask-3"
+agri_output_dir <- output_dir / agri_version
+agri_value <- 1L
 
 # Arrange files
 files <- dplyr::arrange(files, .data[["year"]])
@@ -115,16 +120,17 @@ files <- dplyr::arrange(files, .data[["year"]])
 # Apply agriculture temporal rule 
 file_brick <- restoreutils::reclassify_rule22_temporal_annual_agriculture(
     files                       = files[["path"]],
-    annual_agriculture_class_id = 2,
+    annual_agriculture_class_id = agri_value, # agriculture value for the classified maps
     version                     = agri_version,
     multicores                  = multicores,
     memsize                     = memsize,
-    output_dir                  = output_dir
+    output_dir                  = agri_output_dir
 )
 
+# Split raster brick in multiple files
 agri_files <- restoreutils::reclassify_temporal_results_to_maps(
     years = files[["year"]], 
     output_dir = output_dir, 
     file_brick = file_brick, 
-    agri_version = agri_version
+    version = agri_version
 )
